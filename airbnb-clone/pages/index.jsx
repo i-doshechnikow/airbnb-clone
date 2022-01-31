8e4f52492d67fbd889187a0bfa346454a8b796da
i-doshechnikow <iliya.yurievich@yandex.ru>
2022-01-31 22:11:46 +0300

    edit jsx and delete default layout

---
+++ airbnb-clone/pages/index.jsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <Head>
+        <title>Airbnb</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    <h1>Lets build</h1>
+    </div>
+  )
+}

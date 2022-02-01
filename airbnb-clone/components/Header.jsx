a15343edf3d78599f99ed3770b82a5507ff273cb
i-doshechnikow <iliya.yurievich@yandex.ru>
2022-02-01 21:54:40 +0300

    stylized logo in header

--- airbnb-clone/components/Header.jsx
+++ airbnb-clone/components/Header.jsx
@@ -2,9 +2,9 @@ import Image from 'next/image'
 
 export default function Header() {
   return (
-    <header>
-      {/* left */}
-      <div className="relative flex h-10 items-center">
+    <header className="sticky top-0 z-50 grid grid-cols-3 bg-white p-5 shadow-md md:px-10">
+      {/* left - logo */}
+      <div className="relative my-auto flex h-10 cursor-pointer items-center">
         <Image
           src="https://links.papareact.com/qd3"
           layout="fill"
@@ -13,7 +13,7 @@ export default function Header() {
         />
       </div>
 
-      {/*  middle */}
+      {/*  middle - search */}
       <div></div>
 
       {/* right */}

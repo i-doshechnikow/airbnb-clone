7da8d8ace044e32bb6756288f3dd12cd89352d79
i-doshechnikow <iliya.yurievich@yandex.ru>
2022-02-06 18:06:45 +0300

    add grid for mobile devices

--- airbnb-clone/pages/index.jsx
+++ airbnb-clone/pages/index.jsx
@@ -20,14 +20,16 @@ export default function Home(props) {
       <main className="mx-auto max-w-7xl px-8 sm:px-16">
         <section className="pt-6">
           <h2 className="pb-5 text-4xl font-semibold ">Explore nearby</h2>
-          {exploreData?.map(({ img, distance, location }, id) => (
-            <SmallCard
-              key={id}
-              img={img}
-              location={location}
-              distance={distance}
-            />
-          ))}
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3  xl:grid-cols-4">
+            {exploreData?.map(({ img, distance, location }, id) => (
+              <SmallCard
+                key={id}
+                img={img}
+                location={location}
+                distance={distance}
+              />
+            ))}
+          </div>
           {/* pull data from server */}
         </section>
       </main>

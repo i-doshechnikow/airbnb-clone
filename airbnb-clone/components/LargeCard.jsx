3f752e2dac71b73e984d050112f4387c6289991b
i-doshechnikow <iliya.yurievich@yandex.ru>
2022-02-09 22:00:15 +0300

    work with largecard

--- airbnb-clone/components/LargeCard.jsx
+++ airbnb-clone/components/LargeCard.jsx
@@ -1,5 +1,23 @@
+import Image from 'next/image'
+
 function LargeCard({ img, title, description, buttonText }) {
-  return <div></div>
+  return (
+    <section className="relative cursor-pointer py-16">
+      <div className="relative h-96 min-w-[300px]">
+        <Image
+          className="rounded-2xl"
+          src={img}
+          layout="fill"
+          objectFit="cover"
+        />
+      </div>
+      <div className="absolute top-32 left-12">
+        <h3>{title}</h3>
+        <p>{description}</p>
+        <button>{buttonText}</button>
+      </div>
+    </section>
+  )
 }
 
 export default LargeCard

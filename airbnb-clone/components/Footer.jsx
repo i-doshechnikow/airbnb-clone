4528eba038daf4725da8307f790e315e2d4964b2
i-doshechnikow <iliya.yurievich@yandex.ru>
2022-02-10 19:17:28 +0300

    work with footer

--- airbnb-clone/components/Footer.jsx
+++ airbnb-clone/components/Footer.jsx
@@ -1,5 +1,39 @@
 function Footer() {
-  return <div>Footer</div>
+  return (
+    <div className="grid grid-cols-1 gap-y-10 px-32 py-14 md:grid-cols-4 bg-gray-100 text-gray-600">
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">ABOUT</h5>
+        <p>How airbnb works</p>
+        <p>Newsroom</p>
+        <p>Investors</p>
+        <p>Airbnb plus</p>
+        <p>Airbnb Luxe</p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">COMMUNITY</h5>
+        <p>Accessibility</p>
+        <p>This is not a real site</p>
+        <p>Its a clone</p>
+        <p>Refferals</p>
+        <p>Some info</p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">HOST</h5>
+        <p>React</p>
+        <p>Next</p>
+        <p>Tailwind</p>
+        <p>HTML</p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">SUPPORT</h5>
+        <p>Help centre</p>
+        <p>Trust & Safety</p>
+        <p>Say hi</p>
+        <p>More interesting</p>
+        <p>Be happy!</p>
+      </div>
+    </div>
+  )
 }
 
 export default Footer

b3fef892f86895bef598c127724f14ed16631678
i-doshechnikow <iliya.yurievich@yandex.ru>
2022-02-10 18:52:48 +0300

    added some styles to large card

--- airbnb-clone/components/LargeCard.jsx
+++ airbnb-clone/components/LargeCard.jsx
@@ -12,9 +12,11 @@ function LargeCard({ img, title, description, buttonText }) {
         />
       </div>
       <div className="absolute top-32 left-12">
-        <h3>{title}</h3>
+        <h3 className="mb-3 w-64 text-4xl">{title}</h3>
         <p>{description}</p>
-        <button>{buttonText}</button>
+        <button className="mt-5 rounded-lg bg-gray-900 px-4 py-2 text-sm text-white">
+          {buttonText}
+        </button>
       </div>
     </section>
   )

6ee4871c0dad1bf91a4b3336fcc9d98043bf6e4d
i-doshechnikow <iliya.yurievich@yandex.ru>
2022-02-14 19:07:28 +0300

    work with calendar section

--- airbnb-clone/components/Header.jsx
+++ airbnb-clone/components/Header.jsx
@@ -17,17 +17,19 @@ export default function Header() {
   const [searchInput, setSearchInput] = useState('')
   const [startDate, setStartDate] = useState(new Date())
   const [endDate, setEndDate] = useState(new Date())
+  const [nmbOfGuests, setNmbOfGuests] = useState(2)
+
+  const handleSelect = (event) => {
+    setStartDate(event.selection.startDate)
+    setEndDate(event.selection.endDate)
+  }
 
   let selectionRange = {
     startDate: startDate,
     endDate: endDate,
-    key: 'Selection',
+    key: 'selection',
   }
 
-  const handleSelect = (event) => {
-    setStartDate(event.Selection.startDate)
-    setEndDate(event.Selection.endDate)
-  }
   return (
     <header className="sticky top-0 z-50 grid grid-cols-3 bg-white p-5 shadow-md md:px-10">
       {/* left - logo */}
@@ -63,13 +65,26 @@ export default function Header() {
       </div>
 
       {searchInput && (
-        <div>
+        <div className="col-span-3 mx-auto flex flex-col">
           <DateRangePicker
             ranges={[selectionRange]}
             minDate={new Date()}
             rangeColors={['#FD5B61']}
             onChange={handleSelect}
           />
+          <div className="mb-4 flex items-center border-b">
+            <h2 className="flex-grow text-2xl font-semibold">
+              Number of Guests
+            </h2>
+            <UsersIcon className="h-5" />
+            <input
+              type="number"
+              className="w-12 pl-2 text-red-400 outline-none"
+              value={nmbOfGuests}
+              onChange={(event) => setNmbOfGuests(event.target.value)}
+              min={1}
+            />
+          </div>
         </div>
       )}
     </header>

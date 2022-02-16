09caea880c9d78dabc97ea20f655e05d2d106f20
i-doshechnikow <iliya.yurievich@yandex.ru>
2022-02-16 21:48:30 +0300

    work with search page

--- airbnb-clone/pages/search.jsx
+++ airbnb-clone/pages/search.jsx
@@ -1,7 +1,23 @@
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+
 function Search() {
   return (
-    <div>
-      <h1>Search Page</h1>
+    <div className="h-screen">
+      <Header />
+
+      <main className="flex">
+        <section className="mx-3 w-full">
+          <p className="text-xs">300+ Stays for 5 number of guests</p>
+          <h1 className="mt-2 mb-6 text-3xl font-semibold">Stays Mars</h1>
+
+          <div className='hidden lg:inline-flex'>
+            <p className='px-4 py-2'>Cancellation Flexibility</p>
+          </div>
+        </section>
+      </main>
+
+      <Footer />
     </div>
   )
 }
